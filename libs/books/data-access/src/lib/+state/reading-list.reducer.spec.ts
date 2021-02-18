14a7df2b4d26684879afc5c3ab3b1be838548b63
Rahul Gupta <rahul.gupta11@publicissapient.com>
2021-02-18 11:34:45 +0530

    add code review and fix tests

--- libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
+++ libs/books/data-access/src/lib/+state/reading-list.reducer.spec.ts
@@ -7,7 +7,7 @@ import {
 } from './reading-list.reducer';
 import { createBook, createReadingListItem } from '@tmo/shared/testing';
 
-describe('Books Reducer', () => {
+describe('Reading List Reducer', () => {
   describe('valid Books actions', () => {
     let state: State;
 
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tmo-book-search',
@@ -16,7 +17,7 @@ import { Book } from '@tmo/shared/models';
   styleUrls: ['./book-search.component.scss']
 })
 export class BookSearchComponent implements OnInit {
-  books: ReadingListBook[];
+  books$ : Observable<ReadingListBook[]> = this.store.select(getAllBooks);
 
   searchForm = this.fb.group({
     term: ''
@@ -32,9 +33,6 @@ export class BookSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.store.select(getAllBooks).subscribe(books => {
-      this.books = books;
-    });
   }
 
   formatDate(date: void | string) {
